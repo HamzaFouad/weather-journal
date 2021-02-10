5c246b561fa349c660a0e1edfae62a47954ab4b8
HamzaFouad <HamzaMoFouad@gmail.com>
2021-02-10 19:17:07 +0200

    add todos comments

--- website/app.js
+++ website/app.js
@@ -76,3 +76,64 @@ let updateUI = async () => {
     console.log('error: ', error);
   }
 };
+
+
+/* 
+I was trying here to load countries-codes lookup table from assets/csvfiles/country_codes.csv 
+and create a drop-menu with all countries listed 
+then I would take these countries and convert them to their corresponding country-code
+and pass it with the URL alongside Zipcode.
+But unfortunately, I can not seem to import an module here. So I will leave it for the timebeing as a
+#TODO - countries drop-menu
+
+Another features that are considered to be added
+#TODO - Temperature units menu
+*/
+
+// import parse from 'csv-parse/lib/sync.js';
+// import {readFileSync} from 'fs';
+// import {fs, parse} from '../server.js'
+// import * as path from 'path';
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__dirname);
+
+
+// /* Path vars*/
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const csvfilePath = path.join(__dirname, 'assets', 'csvfiles', 'country_codes.csv');
+
+// let readfileAsJson = (csvfilePath) => {
+//   const recordsJSON = parse(readFileSync(filePath, 'utf8'), {
+//     columns: true,
+//     skip_empty_lines: true
+//   });
+//   return recordsJSON;
+// } 
+
+// let jsonToDict = (recordsJSON) => {
+//   let countryCodeLookUp = {};
+//   for (let i=0; i<recordsJSON.length; i++) {
+//     countryCodeLookUp[recordsJSON[i].Name] = recordsJSON[i].Code;
+//   }
+//   return countryCodeLookUp;
+// }
+
+// let createCountriesMenu = () => {
+//   recordsJSON = readfileAsJson(csvfilePath);
+//   // countryCodeLookUp = jsonToDict(recordsJSON);
+
+//   let selectList = document.getElementById("country");
+
+//   for (let i=0; i<recordsJSON.length; i++) {
+//     selectList.innerHTML += `<option value="${recordsJSON[i].Name}">${recordsJSON[i].Name}</option>`
+//   }
+// }
+// // console.log('app.js is running!');
+// // createCountriesMenu();
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM is fully loaded!');
+// })
